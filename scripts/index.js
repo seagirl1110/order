6114fd08bbf71ab60167e6e4c2b6c8ff2d37f5ec
Elizarova Marina <seagirl1110@gmail.com>
2024-07-11 14:44:24 +0300

    feat: add notification

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,37 @@ const orderContainer = document.querySelector('.order-container');
 const form = orderContainer.querySelector('.form');
 const formInputColl = form.querySelectorAll('.form__input');
 const btnContainer = orderContainer.querySelector('.buttons-container');
+const notificationsContainer = orderContainer.querySelector(
+  '.notifications-container'
+);
+
+const createEl = (tag, className) => {
+  const el = document.createElement(tag);
+  el.setAttribute('class', className);
+  return el;
+};
+
+const createNotification = (title, text, bgColor) => {
+  const notification = createEl('div', 'notification');
+  notification.innerHTML = `
+  <span class="material-symbols-outlined notification__check"> check </span>
+  <h3>${title}</h3>
+  <p>${text}</p>
+  `;
+  notification.style.backgroundColor = bgColor;
+  notificationsContainer.append(notification);
+
+  const closeBtn = createEl(
+    'span',
+    'material-symbols-outlined notification__btn-close'
+  );
+  closeBtn.textContent = 'close';
+  notification.append(closeBtn);
+
+  closeBtn.addEventListener('click', () => {
+    notification.remove();
+  });
+};
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
@@ -11,4 +42,10 @@ form.addEventListener('submit', (evt) => {
   });
 
   btnContainer.classList.add('buttons-container--open');
+
+  createNotification(
+    'Заказ создан',
+    'Ожидайте дальнейшей информации',
+    '#3fc53f'
+  );
 });
